e8c7cc6b4adb6cf097cc18261f31c89a15062639
Artyom Vyatkin <vyatkin.artex@gmail.com>
2019-10-29 22:46:50 +0300

    added a module processing the phone book

---
+++ Tasks_4/Task_4_2/phoneBook.c
@@ -0,0 +1,118 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "phoneBook.h"
+
+const int initialCapacity = 10;
+
+typedef struct PhoneUser PhoneUser;
+struct PhoneUser
+{
+    char name[maxSizeOfName];
+    char phoneNumber[maxSizeOfPhoneNumber];
+};
+
+struct PhoneBook
+{
+    PhoneUser* array;
+    int numberOfUsers;
+    int numberOfUsersInFile;
+    int arrayCapacity;
+};
+
+PhoneBook* initializePhoneBook(char nameOfFile[])
+{
+    char name[maxSizeOfName] = "";
+    char phoneNumber[maxSizeOfPhoneNumber] = "";
+
+    PhoneBook* phoneBook = (PhoneBook*) malloc(sizeof(phoneBook));
+
+    phoneBook->arrayCapacity = initialCapacity;
+    phoneBook->numberOfUsers = 0;
+    phoneBook->numberOfUsersInFile = 0;
+    phoneBook->array = (PhoneUser*) malloc(phoneBook->arrayCapacity * sizeof(PhoneUser));
+
+    FILE* fileInput = fopen(nameOfFile, "a+");
+
+    while (!feof(fileInput))
+    {
+        fscanf(fileInput, "%s %[^\n]", phoneNumber, name);
+
+        addUserToPhoneBook(phoneBook, name, phoneNumber);
+    }
+
+    fclose(fileInput);
+
+    phoneBook->numberOfUsersInFile = phoneBook->numberOfUsers;
+
+    return phoneBook;
+}
+
+void addUserToPhoneBook(PhoneBook* phoneBook, char name[], char phoneNumber[])
+{
+    if (phoneBook->numberOfUsers == phoneBook->arrayCapacity)
+    {
+        phoneBook->arrayCapacity *= 2;
+        phoneBook->array = (PhoneUser*) realloc(phoneBook->array, phoneBook->arrayCapacity * sizeof(PhoneUser));
+    }
+
+    int currentNumberOfUsers = phoneBook->numberOfUsers;
+    strcpy(phoneBook->array[currentNumberOfUsers].name, name);
+    strcpy(phoneBook->array[currentNumberOfUsers].phoneNumber, phoneNumber);
+    phoneBook->numberOfUsers++;
+}
+
+bool getPhoneByName(PhoneBook* phoneBook, char name[], char desiredPhoneNumber[])
+{
+    bool isStringsEqual = false;
+    int strcmpReturn = 0;
+    for (int i = 0; i < phoneBook->numberOfUsers; i++)
+    {
+        strcmpReturn = strcmp(phoneBook->array[i].name, name);
+        isStringsEqual = (strcmpReturn == 0);
+        if (isStringsEqual)
+        {
+            strcpy(desiredPhoneNumber, phoneBook->array[i].phoneNumber);
+            return true;
+        }
+    }
+
+    return false;
+}
+
+bool getNameByPhone(PhoneBook* phoneBook, char desiredName[], char phoneNumber[])
+{
+    bool isStringsEqual = false;
+    int strcmpReturn = 0;
+    for (int i = 0; i < phoneBook->numberOfUsers; i++)
+    {
+        strcmpReturn = strcmp(phoneBook->array[i].phoneNumber, phoneNumber);
+        isStringsEqual = (strcmpReturn == 0);
+        if (isStringsEqual)
+        {
+            strcpy(desiredName, phoneBook->array[i].name);
+            return true;
+        }
+    }
+
+    return false;
+}
+
+void writeDataToFile(PhoneBook* phoneBook, FILE* fileOutput)
+{
+    for (int i = phoneBook->numberOfUsersInFile; i < phoneBook->numberOfUsers; i++)
+    {
+        fprintf(fileOutput, "%s %s\n", phoneBook->array[i].phoneNumber, phoneBook->array[i].name);
+    }
+
+    phoneBook->numberOfUsersInFile = phoneBook->numberOfUsers;
+}
+
+void deletedPhoneBook(PhoneBook* phoneBook)
+{
+    free(phoneBook->array);
+    free(phoneBook);
+}
+
+
+
---
+++ Tasks_4/Task_4_2/phoneBook.h
@@ -0,0 +1,39 @@
+/**
+ * The module supports file data processing.
+ * Data about one user should be written in a file in the form:
+ * "<phone (a string without spaces, tabs, newline)> <username (spaces are allowed)><newline>".
+ */
+
+#ifndef PHONEBOOK_H
+#define PHONEBOOK_H
+
+#include <stdbool.h>
+
+/*const int maxSizeOfName = 40;
+const int maxSizeOfPhoneNumber = 20;*/
+
+#define maxSizeOfName 40
+#define maxSizeOfPhoneNumber 20
+
+
+typedef struct PhoneBook PhoneBook;
+struct PhoneBook;
+
+/* nameOfFile contains the name of file with data about users.
+ * If the file is missing, then it's created. */
+PhoneBook* initializePhoneBook(char nameOfFile[]);
+
+void addUserToPhoneBook(PhoneBook* phoneBook, char name[], char phoneNumber[]);
+
+/* The phone number will be placed in the desiredPhoneNumber. */
+bool getPhoneByName(PhoneBook* phoneBook, char name[], char desiredPhoneNumber[]);
+
+/* The name will be placed in the desiredName. */
+bool getNameByPhone(PhoneBook* phoneBook, char desiredName[], char phoneNumber[]);
+
+/* The file should be opened with the following access mode flags: "a", "a+". */
+void writeDataToFile(PhoneBook* phoneBook, FILE* fileOutput);
+
+void deletedPhoneBook(PhoneBook* phoneBook);
+
+#endif //PHONEBOOK_H
