fe57b2c19f11e331cc1db97e72f0961b91a3bafe
Artyom Vyatkin <vyatkin.artex@gmail.com>
2019-11-12 01:05:35 +0300

    added a module that implements the stack

---
+++ Tasks_5/stack.c
@@ -0,0 +1,84 @@
+#include <stdlib.h>
+#include "stack.h"
+
+typedef struct StackElement StackElement;
+struct StackElement
+{
+    double value;
+    StackElement* next;
+};
+
+struct Stack
+{
+    StackElement* top;
+    int size;
+};
+
+Stack* createStack()
+{
+    Stack* stack = (Stack*) malloc(sizeof(Stack));
+    stack->top = NULL;
+    stack->size = 0;
+    return stack;
+}
+
+StackElement* createStackElement(double value, StackElement* nextElement)
+{
+    StackElement* stackElement = (StackElement*) malloc(sizeof(StackElement));
+    stackElement->value = value;
+    stackElement->next = nextElement;
+    return stackElement;
+}
+
+void pushToStack(Stack* stack, double value)
+{
+    StackElement* newElement = createStackElement(value, stack->top);
+    stack->top = newElement;
+    stack->size++;
+}
+
+double popFromStack(Stack* stack)
+{
+    if (isStackEmpty(stack))
+    {
+        return 0.0;
+    }
+
+    StackElement* poppedElement = stack->top;
+    double returnValue = poppedElement->value;
+    stack->top = poppedElement->next;
+    free(poppedElement);
+    stack->size--;
+
+    return returnValue;
+}
+
+double peekIntoStack(Stack* stack)
+{
+    if (isStackEmpty(stack))
+    {
+        return 0.0;
+    }
+
+    return stack->top->value;
+}
+
+bool isStackEmpty(Stack* stack)
+{
+    return stack->top == 0;
+}
+
+void deleteStack(Stack* stack)
+{
+    while (!isStackEmpty(stack))
+    {
+        popFromStack(stack);
+    }
+
+    free(stack);
+}
+
+int getStackSize(Stack* stack)
+{
+    return stack->size;
+}
---
+++ Tasks_5/stack.h
@@ -0,0 +1,25 @@
+#ifndef STACK_H
+#define STACK_H
+
+#include <stdbool.h>
+
+typedef struct Stack Stack;
+struct Stack;
+
+Stack* createStack();
+
+void pushToStack(Stack* stack, double value);
+
+/* If the stack is empty, the function will return 0.0 */
+double popFromStack(Stack* stack);
+
+/* If the stack is empty, the function will return 0.0 */
+double peekIntoStack(Stack* stack);
+
+bool isStackEmpty(Stack* stack);
+
+void deleteStack(Stack* stack);
+
+int getStackSize(Stack* stack);
+
+#endif //STACK_H
