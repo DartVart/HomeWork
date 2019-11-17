8167a497c34343a2b5ea2cb423a47ae7fe67fd3b
Artyom Vyatkin <vyatkin.artex@gmail.com>
2019-11-17 21:13:17 +0300

    changed an array of char to an array of numbres that represented partition

--- Tasks_2/Task_2_1.c
+++ Tasks_2/Task_2_1.c
@@ -1,55 +1,34 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
-
-const int maxSizeOfAddingString = 10;
 
 int getMinimum(int a, int b)
 {
     return (a < b) ? a : b;
 }
 
-//This method doesn't change amount of memory allocated for the string
-void shortenString(char string[], int newLengthOfString)
+void printPartitionOfNumber(int number, int maximumTermInPartition, int partition[], int currentNumberOfTerms)
 {
-    string[newLengthOfString] = '\0';
-}
-
-void printPartitionOfNumber(int number, int maximumTermInPartition, char* partOfPartition, int lengthOfPreviousAddingString)
-{
-    int lengthOfPartOfPartition = strlen(partOfPartition);
-    int lengthOfPartitionWithoutLastTerm = lengthOfPartOfPartition - lengthOfPreviousAddingString;
-
     if (number == 0)
     {
-        int lengthOfPartitionWithoutLastPlus = lengthOfPartOfPartition - 2;
-        shortenString(partOfPartition, lengthOfPartitionWithoutLastPlus);
+        // Display the current partition.
+        for (int i = 0; i < currentNumberOfTerms - 1; i++)
+        {
+            printf("%d + ", partition[i]);
+        }
+        printf("%d\n", partition[currentNumberOfTerms - 1]);
 
-        printf("%s\n", partOfPartition);
-
-        shortenString(partOfPartition, lengthOfPartitionWithoutLastTerm);
         return;
     }
 
-    int lengthOfExtendedPartOfPartition = 0;
-    int lengthOfAddingString = 0;
-    int maximumTermInCurrentPartOfPartition = getMinimum(number, maximumTermInPartition);
-    for (int term = 1; term <= maximumTermInCurrentPartOfPartition; term++)
+    currentNumberOfTerms++;
+
+    int maximumInCurrentPartOfPartition = getMinimum(number, maximumTermInPartition);
+    for (int term = 1; term <= maximumInCurrentPartOfPartition; term++)
     {
-        char addingString[maxSizeOfAddingString] = "";
-        itoa(term, addingString, 10);
-        strcat(addingString, " + ");
-        lengthOfAddingString = strlen(addingString);
+        partition[currentNumberOfTerms - 1] = term;
 
-        lengthOfExtendedPartOfPartition = lengthOfPartOfPartition + lengthOfAddingString + 1;
-        partOfPartition = (char*) realloc(partOfPartition, (lengthOfExtendedPartOfPartition) * sizeof(char));
-
-        strcat(partOfPartition, addingString);
-
-        printPartitionOfNumber(number - term, term, partOfPartition, lengthOfAddingString);
+        printPartitionOfNumber(number - term, term, partition, currentNumberOfTerms);
     }
-
-    shortenString(partOfPartition, lengthOfPartitionWithoutLastTerm);
 }
 
 int main()
@@ -60,11 +39,10 @@ int main()
 
     printf("Partition of %d:\n ", inputNumber);
 
-    char* partition = (char*) calloc(0 , sizeof(char));
+    int* partition = (int*) calloc(inputNumber, sizeof(int));
     printPartitionOfNumber(inputNumber, inputNumber - 1, partition, 0);
 
     free(partition);
 
     return 0;
 }
-
