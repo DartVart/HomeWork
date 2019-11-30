b25b708387bb2c0fe7f12c8e351abcf7ea09e96b
Artyom Vyatkin <vyatkin.artex@gmail.com>
2019-11-30 18:01:05 +0300

    fixed code design weaknesses

--- Tasks_4/Task_4_2/stringReading.h
+++ Tasks_4/Task_4_2/stringReading.h
@@ -7,7 +7,6 @@
 #include <stdbool.h>
 #include <stdio.h>
 
- /* The function reads a non-empty string until a '\n' is found, not including it.
-  * The function will return 'false' if the file ended before reading a string or (inputStream == NULL). */
+/* The function reads a non-empty string until a '\n' is found, not including it.
+ * The function will return 'false' if the file ended before reading a string or (inputStream == NULL). */
 bool scanStringWithSpaces(FILE* inputStream, char* stringBuffer, int maxLengthOfString);
-
