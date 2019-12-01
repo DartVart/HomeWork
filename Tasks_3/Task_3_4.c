74f1c8d0f7a418042e6163a9dae78ebc90b8b104
Artyom Vyatkin <vyatkin.artex@gmail.com>
2019-12-02 01:49:34 +0300

    Added input data check for NULL; broke functions into separate ones

--- Tasks_3/Task_3_4.c
+++ Tasks_3/Task_3_4.c
@@ -1,4 +1,103 @@
 #include <stdio.h>
+#include <stdbool.h>
+
+bool enterInitialData(int* numberOfUsers, int* requiredNumberOfActions, int* requiredTimeInterval)
+{
+    printf("Enter the number of users:\n");
+    bool isCorrectInput = scanf("%d", numberOfUsers) != 0;
+    if (!isCorrectInput || *numberOfUsers <= 0)
+    {
+        printf("Incorrect input!");
+        return false;
+    }
+
+    printf("Enter the required number of actions:\n");
+    isCorrectInput = scanf("%d", requiredNumberOfActions) != 0;
+    if (!isCorrectInput || *requiredNumberOfActions < 0)
+    {
+        printf("Incorrect input!");
+        return false;
+    }
+
+    printf("Enter the required time period:\n");
+    isCorrectInput = scanf("%d", requiredTimeInterval) != 0;
+    if (!isCorrectInput || *requiredTimeInterval <= 0)
+    {
+        printf("Incorrect input!");
+        return false;
+    }
+    return true;
+}
+
+bool getNumberOfSuitableActions(int numberOfUserActions, int requiredTimeInterval, int* counterOfSuitableActions)
+{
+    int timeIntervalAfterAction = 0;
+    bool isCorrectInput = true;
+
+    for (int i = 0; i < numberOfUserActions; i++)
+    {
+        isCorrectInput = scanf("%d", &timeIntervalAfterAction) != 0;
+        if (!isCorrectInput || timeIntervalAfterAction <= 0)
+        {
+            return false;
+        }
+
+        if (timeIntervalAfterAction <= requiredTimeInterval)
+        {
+            (*counterOfSuitableActions)++;
+        }
+    }
+    return true;
+}
+
+bool processUserActivityData(int requiredTimeInterval, int* counterOfSuitableActions, int indexOfUser)
+{
+    int numberOfUserActions = 0;
+
+    bool isCorrectInput = true;
+    printf("Enter the number actions of user %d:\n", indexOfUser);
+    isCorrectInput = scanf("%d", &numberOfUserActions) != 0;
+    if (!isCorrectInput || numberOfUserActions < 0)
+    {
+        return false;
+    }
+
+    if (numberOfUserActions != 0)
+    {
+        printf("Enter %d time intervals indicating how many minutes ago these actions happened:\n",
+               numberOfUserActions);
+    }
+
+    isCorrectInput = getNumberOfSuitableActions(numberOfUserActions, requiredTimeInterval, counterOfSuitableActions);
+
+    return isCorrectInput;
+}
+
+bool getNumberOfSuitableUsers(int numberOfUsers, int requiredNumberOfActions, int requiredTimeInterval,
+                              int* numberOfSuitableUsers)
+{
+    int counterOfSuitableActions = 0;
+
+    bool isCorrectInput = true;
+
+    for (int i = 1; i <= numberOfUsers; i++)
+    {
+        isCorrectInput = processUserActivityData(requiredTimeInterval, &counterOfSuitableActions, i);
+        if (!isCorrectInput)
+        {
+            printf("Incorrect input!");
+            return false;
+        }
+
+        if (counterOfSuitableActions == requiredNumberOfActions)
+        {
+            (*numberOfSuitableUsers)++;
+        }
+        counterOfSuitableActions = 0;
+    }
+
+    return numberOfSuitableUsers;
+}
 
 int main()
 {
@@ -6,43 +105,24 @@ int main()
     int requiredNumberOfActions = 0;
     int requiredTimeInterval = 0;
     int numberOfSuitableUsers = 0;
-    int numberOfUserActions = 0;
-    int timeIntervalAfterAction = 0;
-    int counterOfSuitableActions = 0;
 
-    printf("Enter the number of users: ");
-    scanf("%d", &numberOfUsers);
-    printf("Enter the required number of actions: ");
-    scanf("%d", &requiredNumberOfActions);
-    printf("Enter the required time period:");
-    scanf("%d", &requiredTimeInterval);
+    bool isCorrectInput = true;
 
-
-    for (int i = 1; i <= numberOfUsers; i++)
+    isCorrectInput = enterInitialData(&numberOfUsers, &requiredNumberOfActions, &requiredTimeInterval);
+    if (!isCorrectInput)
     {
-        printf("Enter the number actions of user %d:", i);
-        scanf("%d", &numberOfUserActions);
+        return -1;
+    }
 
-        printf("Enter %d time intervals indicating how many minutes ago these actions happened: ", numberOfUserActions);
-        for (int j = 0; j < numberOfUserActions; j++)
-        {
-            scanf("%d", &timeIntervalAfterAction);
-            if (timeIntervalAfterAction <= requiredTimeInterval)
-            {
-                counterOfSuitableActions++;
-            }
-        }
-
-        if (counterOfSuitableActions == requiredNumberOfActions)
-        {
-            numberOfSuitableUsers++;
-        }
-        counterOfSuitableActions = 0;
-
+    isCorrectInput = getNumberOfSuitableUsers(numberOfUsers, requiredNumberOfActions, requiredTimeInterval,
+                                              &numberOfSuitableUsers);
+    if (!isCorrectInput)
+    {
+        return -1;
     }
 
     printf("The number of users who completed exactly %d actions in the last %d minute(-s): %d",
-            requiredNumberOfActions, requiredTimeInterval, numberOfSuitableUsers);
+           requiredNumberOfActions, requiredTimeInterval, numberOfSuitableUsers);
 
     return 0;
 }
