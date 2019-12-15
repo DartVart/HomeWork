4c42d1d1d220943874e9a57be8e0da0cd9ec82b8
Artyom Vyatkin <vyatkin.artex@gmail.com>
2019-12-15 22:10:28 +0300

    added module that scans strings

---
+++ Tasks_9/Task_9_1/stringReading.c
@@ -0,0 +1,67 @@
+#include <string.h>
+#include "stringReading.h"
+
+/* The function moves the stream position indicator to the first character
+ * that is not equal to the character recorded in the 'symbol'.
+ * The function returns false if the end of the file is reached or (inputStream == NULL). */
+bool ignoreSymbolsInInputStream(char* symbols, FILE* inputStream)
+{
+    if (inputStream == NULL)
+    {
+        return false;
+    }
+
+    int readingSymbol = 0;
+    do
+    {
+        readingSymbol = fgetc(inputStream);
+        if (readingSymbol == EOF)
+        {
+            return false;
+        }
+    } while (strchr(symbols, readingSymbol) != NULL);
+
+    ungetc(readingSymbol, inputStream);
+    return true;
+}
+
+bool scanDelimitedString(char* delimiters, FILE* inputStream, char* stringBuffer,
+                                     int maxLengthOfString)
+{
+    bool isReadingCorrect = ignoreSymbolsInInputStream(delimiters, inputStream);
+    if (!isReadingCorrect)
+    {
+        return false;
+    }
+
+    int readingSymbolKey = 0;
+    int currentLength = 0;
+    readingSymbolKey = fgetc(inputStream);
+
+    do
+    {
+        stringBuffer[currentLength] = (char)readingSymbolKey;
+        currentLength++;
+        readingSymbolKey = fgetc(inputStream);
+
+    } while (readingSymbolKey != EOF &&
+             strchr(delimiters, readingSymbolKey) == NULL &&
+             currentLength < maxLengthOfString - 1);
+
+    stringBuffer[currentLength] = '\0';
+    return true;
+}
+
+bool scanStringWithSpaces(FILE* inputStream, char* stringBuffer, int maxLengthOfString)
+{
+    bool isCorrectScanning = scanDelimitedString("\n\t", inputStream,
+                                                 stringBuffer, maxLengthOfString);
+    return isCorrectScanning;
+}
+
+bool scanString(FILE* inputStream, char* stringBuffer, int maxLengthOfString)
+{
+    bool isCorrectScanning = scanDelimitedString("\n\t ", inputStream,
+                                                 stringBuffer, maxLengthOfString);
+    return isCorrectScanning;
+}
---
+++ Tasks_9/Task_9_1/stringReading.h
@@ -0,0 +1,16 @@
+/**
+ * This module implements safe reading a string (char array) with spaces
+ * */
+
+#pragma once
+
+#include <stdbool.h>
+#include <stdio.h>
+
+ /* The function reads a non-empty string until a '\n' or is found, not including it.
+  * The function will return 'false' if the file ended before reading a string or (inputStream == NULL). */
+bool scanStringWithSpaces(FILE* inputStream, char* stringBuffer, int maxLengthOfString);
+
+/* The function reads a non-empty string until a '\n' or '\t' or ' ' is found, not including it.
+ * The function will return 'false' if the file ended before reading a string or (inputStream == NULL). */
+bool scanString(FILE* inputStream, char* stringBuffer, int maxLengthOfString);
