b0c99ec7e8d3c1f635c7ba7594b5153ea0a57cf4
Artyom Vyatkin <vyatkin.artex@gmail.com>
2020-03-29 23:05:09 +0300

    changed the names of functions and variables to more correct

--- src/main/kotlin/homeworks/homework1/task1/task1.kt
+++ src/main/kotlin/homeworks/homework1/task1/task1.kt
@@ -6,45 +6,46 @@ import java.util.Scanner
 fun scanPositiveInteger(): Int {
     val scan = Scanner(System.`in`)
     var number = scan.nextInt()
-    while (number < 1) {
-        println("Please, enter a positive integer.")
+    while (number < 0) {
+        println("Please, enter a non-negative integer.")
         number = scan.nextInt()
     }
     return number
 }
 
 /**
- * Tries to read an array from the console until the array is entered correctly
+ * Tries to read an list from the console until the list is entered correctly
  * */
 fun scanIntList(delimiters: Regex = Regex(" +")): List<Int> {
-    var scannedArray: List<Int>? = null
-    while (scannedArray == null) {
+    var scannedList: List<Int>? = null
+    while (scannedList == null) {
         try {
-            scannedArray = readLine()?.trim()?.split(delimiters)?.map { it.toInt() }
+            scannedList = readLine()?.trim()?.split(delimiters)?.map { it.toInt() }
         } catch (exception: NumberFormatException) {
             println("Please, enter a string of integers only.")
             continue
         }
-        if (scannedArray == null) {
+        if (scannedList == null) {
             println("Please, try to enter the string of integers again.")
         }
     }
-    return scannedArray
+    return scannedList
 }
 
 fun changePartOfList(firstPartLength: Int, secondPartLength: Int, list: MutableList<Int>) {
+    val listSize = firstPartLength + secondPartLength
     when {
         firstPartLength < 0 || secondPartLength < 0 -> {
             throw IndexOutOfBoundsException("The size of the list part is negative")
         }
-        firstPartLength + secondPartLength != list.size -> {
+        listSize != list.size -> {
             throw IndexOutOfBoundsException("The list size doesn't equal to the sum of its parts")
         }
     }
 
     list.subList(0, firstPartLength).reverse()
-    list.subList(firstPartLength, list.size).reverse()
-    list.subList(0, list.size).reverse()
+    list.subList(firstPartLength, listSize).reverse()
+    list.reverse()
 }
 
 fun main() {
