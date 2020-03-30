5eb0e1d6494c453da3543673c0f1a402166c0f0c
Artyom Vyatkin <vyatkin.artex@gmail.com>
2020-03-30 17:20:13 +0300

    added a solution to task 1.2

---
+++ src/main/kotlin/homeworks/homework1/task2/task2.kt
@@ -0,0 +1,42 @@
+package homeworks.homework1.task2
+
+import java.util.Scanner
+
+fun scanNonNegativeInteger(): Int {
+    val scan = Scanner(System.`in`)
+    var number = scan.nextInt()
+    while (number < 0) {
+        println("Please, enter a non-negative integer.")
+        number = scan.nextInt()
+    }
+    return number
+}
+
+fun getRecursiveFactorial(number: Int): Int {
+    if (number < 0) {
+        throw IllegalArgumentException("The number must be non-negative")
+    }
+
+    return if (number == 0) 1 else getRecursiveFactorial(number - 1) * number
+}
+
+fun getIterativeFactorial(number: Int): Int {
+    if (number < 0) {
+        throw IllegalArgumentException("The number must be non-negative")
+    }
+
+    var result = 1
+    for (i in 2..number) {
+        result *= i
+    }
+    return result
+}
+
+fun main() {
+    println("Enter a non-negative number:")
+    val inputNumber = scanNonNegativeInteger()
+
+    println("Result:")
+    println("$inputNumber! = ${getRecursiveFactorial(inputNumber)} (recursive)")
+    println("$inputNumber! = ${getIterativeFactorial(inputNumber)} (iterative)")
+}
