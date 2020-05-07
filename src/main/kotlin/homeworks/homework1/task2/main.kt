3bbfbad41e422da7c71952f7564b9526b5f9382b
Artyom Vyatkin <vyatkin.artex@gmail.com>
2020-05-07 14:31:24 +0300

    Added catching some exceptions

--- src/main/kotlin/homeworks/homework1/task2/main.kt
+++ src/main/kotlin/homeworks/homework1/task2/main.kt
@@ -13,16 +13,16 @@ fun scanNonNegativeInteger(): Int {
 }
 
 fun getRecursiveFactorial(number: Int): Int {
-    if (number < 0) {
-        throw IllegalArgumentException("The number must be non-negative")
+    require(number >= 0) {
+        "The number must be non-negative"
     }
 
     return if (number == 0) 1 else getRecursiveFactorial(number - 1) * number
 }
 
 fun getIterativeFactorial(number: Int): Int {
-    if (number < 0) {
-        throw IllegalArgumentException("The number must be non-negative")
+    require(number >= 0) {
+        "The number must be non-negative"
     }
 
     var result = 1
@@ -36,7 +36,10 @@ fun main() {
     println("Enter a non-negative number:")
     val inputNumber = scanNonNegativeInteger()
 
-    println("Result:")
-    println("$inputNumber! = ${getRecursiveFactorial(inputNumber)} (recursive)")
-    println("$inputNumber! = ${getIterativeFactorial(inputNumber)} (iterative)")
+    try {
+        println("Factorial of $inputNumber: ${getRecursiveFactorial(inputNumber)} (recursive)")
+        println("Factorial of $inputNumber: ${getIterativeFactorial(inputNumber)} (iterative)")
+    } catch (exception: IllegalArgumentException) {
+        println("Error: ${exception.message}")
+    }
 }
