0d7b2f0381784a946c1958c7985b65f5b8c4291c
Artyom Vyatkin <vyatkin.artex@gmail.com>
2020-06-02 16:11:15 +0300

    removed extra space

--- src/main/kotlin/tests/test2/task1/IntComparator.kt
+++ src/main/kotlin/tests/test2/task1/IntComparator.kt
@@ -1,6 +1,6 @@
 package tests.test2.task1
 
-class IntComparator: Comparator<Int> {
+class IntComparator : Comparator<Int> {
     override fun compare(firstInteger: Int?, secondInteger: Int?): Int {
         if (firstInteger != null && secondInteger != null) {
             return when {
